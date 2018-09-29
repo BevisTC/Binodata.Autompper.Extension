f2d4d307f2ce231d3eadcba5310d3cc9b8f5ac04
Bevis <bevis@binodata.com>
2018-09-29 17:27:11 +0800

    start a project and add auto mapper config

---
+++ Core/Binodata.Automapper.Extension/AutoMapperConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using AutoMapper;
+
+namespace Binodata.Automapper.Extension
+{
+    public static class AutoMapperConverter
+    {
+        /// <summary>
+        /// Convert to Model 
+        /// </summary>
+        /// <typeparam name="From"></typeparam>
+        /// <typeparam name="To"></typeparam>
+        /// <param name="config"></param>
+        /// <param name="source"></param>
+        /// <param name="destination"></param>
+        /// <returns></returns>
+        public static To ConvertModel<From, To>(this MapperConfiguration config, From source, To destination)
+        {
+            IMapper mapper = config.CreateMapper();
+            return mapper.Map<From, To>(source, destination);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="From"></typeparam>
+        /// <typeparam name="To"></typeparam>
+        /// <param name="config"></param>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static To ConvertModel<From, To>(this MapperConfiguration config, From source)
+        {
+            IMapper mapper = config.CreateMapper();
+            return mapper.Map<From, To>(source);
+
+        }
+    }
+}
